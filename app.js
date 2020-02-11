0206889d50eef56796dd96c3c82761bbe1c2b847
Rui Rodrigues <48014356+ruifsr@users.noreply.github.com>
2020-02-11 18:02:54 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
 app.use('/', express.static('www'));
-app.listen(process.env.PORT ? process.env.PORT : 80, () => console.log('redirect active on port 80'));
+const port = process.env.PORT ? process.env.PORT : 80;
+app.listen(port, () => console.log('Listening on port', port));
