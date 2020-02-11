7f2df6a4bdb03b66e9e2459a55a5cfeb6d1915dc
Rui Rodrigues <48014356+ruifsr@users.noreply.github.com>
2020-02-11 17:58:08 +0000

    teste3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
 app.use('/', express.static('www'));
-app.listen(process.env.PORT, () => console.log('redirect active on port 80'));
+app.listen(process.env.PORT ? process.env.PORT : 80, () => console.log('redirect active on port 80'));
